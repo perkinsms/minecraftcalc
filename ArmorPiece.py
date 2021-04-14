13ec401d1705011e072bf387ce886cd42c47cf61
Michael Perkins <perkinsms@gmail.com>
2021-04-14 16:12:08 -0400

    added function to the Armor and ArmorPiece classes

--- ArmorPiece.py
+++ ArmorPiece.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+import yaml
+from math import floor
+
+with open('defense.yaml') as file:
+    defense=yaml.load(file, Loader=yaml.FullLoader)
+
+with open('epf.yaml') as file:
+    enchant_factor = yaml.load(file, Loader=yaml.FullLoader)
+
 class ArmorPiece:
-    def __init__(material=None, enchantment=None):
+    def __init__(shape=None, material=None, enchantment=None, ench_lev=None):
+        self.shape = shape
         self.material = material 
         self.enchantment = enchantment
+        self.ench_lev = ench_lev
+
+    def value(self):
+        return (defense[self.shape][self.material] * 4)
+
+    def epf(self):
+        return enchant_factor[self.enchantment]["level"][self.ench_lev]
